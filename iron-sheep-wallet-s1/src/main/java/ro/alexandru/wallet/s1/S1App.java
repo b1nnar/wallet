e0f73067539cd1f6509c20f78d6a5379db03f08c
Alexandru Piticas <piticas.alexandru@gmail.com>
2021-03-08 00:22:14 +0200

    Fix

--- iron-sheep-wallet-s1/src/main/java/ro/alexandru/wallet/s1/S1App.java
+++ iron-sheep-wallet-s1/src/main/java/ro/alexandru/wallet/s1/S1App.java
@@ -4,7 +4,7 @@ import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ro.alexandru.wallet.domain.model.event.WalletOperation;
+import ro.alexandru.wallet.domain.model.event.WalletOperationResult;
 import ro.alexandru.wallet.messaging.consumer.KafkaMessageConsumer;
 import ro.alexandru.wallet.messaging.consumer.KafkaMessageConsumerConfig;
 import ro.alexandru.wallet.messaging.consumer.MessageConsumer;
@@ -35,9 +35,9 @@ public class S1App {
                 "localhost:9092", "s1-consumer", "TOPIC.S2.01"
         );
 
-        MessageConsumer<WalletOperation> messageConsumer = new KafkaMessageConsumer<>(kafkaMessageConsumerConfig, new JSONDeserializer<>(WalletOperation.class));
-        MessageProcessor<WalletOperation> messageProcessor = value -> LOG.info("S1 Consumer processed value: `{}`", value);
-        MessageConsumerProcess<WalletOperation> messageConsumerProcess = new MessageConsumerProcess<>("S1 Consumer", messageConsumer, messageProcessor);
+        MessageConsumer<WalletOperationResult> messageConsumer = new KafkaMessageConsumer<>(kafkaMessageConsumerConfig, new JSONDeserializer<>(WalletOperationResult.class));
+        MessageProcessor<WalletOperationResult> messageProcessor = value -> LOG.info("S1 Consumer processed value: `{}`", value);
+        MessageConsumerProcess<WalletOperationResult> messageConsumerProcess = new MessageConsumerProcess<>("S1 Consumer", messageConsumer, messageProcessor);
 
         messageConsumerProcess.start();
     }
